927fc66966ece9cc591a6da4ab33b283393888c2
ssad3846-commits <ssad3846@icloud.com>
2025-12-10 10:31:33 +0300

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.saad.trendmadinah',
+  appId: 'trendmadinah',
   appName: 'ترند المدينة',
   webDir: 'dist/public',
   server: {
